b2358ac1b0efab01f2180d330becb3dd5cb7814e
Refat Ahmed Prio <refatahmed@iut-dhaka.edu>
2025-08-04 23:45:25 +0600

    Add files via upload

---
+++ Leap year.py
@@ -0,0 +1,10 @@
+x = int(input("Enter a year: "))
+
+if x%400 == 0:
+    print(f"{x} is a leap year")
+elif x%100 == 0:
+    print(f"{x} is not a leap year")
+elif x%4 == 0:
+    print(f"{x} is a leap year")
+else:
+    print(f"{x} is not a leap year")
